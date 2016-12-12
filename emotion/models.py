5d445e169ddb03770e67ebbbdefa827f33de31b6
Ashutosh Das <git.pyprism@gmail.com>
2016-12-13 01:28:40 +0600

    added model

--- emotion/models.py
+++ emotion/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Emotion(models.Model):
+    emotion_option = (
+        ('Sa', 'Sad'),
+        ('An', 'Angry'),
+        ('Co', 'Confused'),
+        ('Ha', 'Happy'),
+        ('De', 'Depressed'),
+        ('Ax', 'anxious'),  # thanks to hiren  :P  lol
+        ('Wo', 'worried'),
+        ('Fr', 'frightening'),
+    )
+    state = models.CharField(max_length=2, choices=emotion_option)
+    reason = models.CharField(max_length=1000, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
