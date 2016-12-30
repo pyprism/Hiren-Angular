75c40f14f7803cce79bbf0e5919e79eea6fa0e54
Ashutosh Das <git.pyprism@gmail.com>
2016-12-30 21:03:12 +0600

    added new  chart

--- static/js/chart_all.js
+++ static/js/chart_all.js
@@ -1,6 +1,63 @@
 /**
  * Created by prism on 12/16/16.
  */
-$( document ).ready(function() {
-    var ctx = $("chart_full");
+$(function () {
+    $.ajax('/chart/', {
+        contentType: 'application/json',
+        success: function(data) {
+            console.log(data);
+            var myChart = Highcharts.chart('chart_full', {
+                chart: {
+                    type: 'spline'
+                },
+                zoomType: 'x',
+                title: {
+                    text: 'Yearly Emotional Graph'
+                },
+                subtitle: {
+                    text: 'Current Year'
+                },
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+                        'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                //yAxis: {
+                //    title: {
+                //        text: 'Percentage %'
+                //    }
+                //},
+                series: data
+            });
+        },
+        error: function(data) {
+            console.error(data);
+        }
+    });
+    /*var myChart = Highcharts.chart('chart_full', {
+     chart: {
+     type: 'line'
+     },
+     title: {
+     text: 'Yearly Emotional Graph'
+     },
+     subtitle: {
+     text: 'Current Year'
+     },
+     xAxis: {
+     categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+     'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+     },
+     yAxis: {
+     title: {
+     text: 'Fruit eaten'
+     }
+     },
+     series: [{
+     name: 'Jane',
+     data: [1, 0, 4, 9]
+     }, {
+     name: 'John',
+     data: [5, 7, 3]
+     }]
+     });*/
 });
